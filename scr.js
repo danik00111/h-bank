cd4482b37f9f77ba479b830f8c2c80a7d850bc62
danik00111 <kremenskiydaniil@gmail.com>
2023-06-21 19:30:36 +0300

    changed the appearance of debug logs

--- scr.js
+++ scr.js
@@ -1,15 +1,15 @@
 let likes = document.querySelectorAll('.S_likes-wrap');
 let likesCount;
-likes.forEach(like => {
+likes.forEach((like,index) => {
   like.addEventListener('click', () => {
     if(like.classList.contains('active')){
       like.classList.remove('active');
-      console.log(`.active removed for ${like}`)
+      console.log(`${index} --`)
     }else{
       like.classList.add('active');
-      console.log(`.active added for ${like}`)
+      console.log(`${index} ++`)
     }
-    console.log(`.active toggled for ${like}`)
+    console.log(`${index} !`)
   });
 });
 
